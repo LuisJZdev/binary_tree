76fa8980cf3e2e08f9bb72527f7d53ac866ebcf8
Eric Clapton <bunerboveda@proton.me>
2024-03-17 18:19:54 -0400

    remove elements. We are done by now

--- binary_tree.py
+++ binary_tree.py
@@ -22,6 +22,27 @@ class BinaryTree:
 
         recursion(node=self, value=value)
 
+    def __add_node(self, node):
+
+        def recursion(node: BinaryTree, value) -> None:
+
+            if node.root_value == None:
+                node.root_value = value.root_value
+                node.left = value.left
+                node.right = value.right
+            elif value.root_value < node.root_value:
+                if node.left == None:
+                    node.left = value
+                else:
+                    recursion(node=node.left, value=value)
+            elif value.root_value >= node.root_value:
+                if node.right == None:
+                    node.right = value
+                else:
+                    recursion(node=node.right, value=value)
+
+        recursion(node=self, value=node)
+
     def search(self, value) -> bool:
 
         def recursion(node: BinaryTree, value) -> bool:
@@ -35,7 +56,7 @@ class BinaryTree:
                 return recursion(node=node.right, value=value)
 
         return recursion(node=self, value=value)
-    
+
     def show(self) -> None:
 
         def recursion(node: BinaryTree, space=0) -> None:
@@ -49,3 +70,41 @@ class BinaryTree:
                 recursion(node=node.right, space=space+1)
         
         recursion(node=self)
+
+    def remove(self, value) -> None:
+
+        def recursion(node: BinaryTree, value) -> None:
+            if node.root_value == value:
+
+                left = node.left
+                right = node.right
+
+                node.root_value = None
+                node.left = None
+                node.right = None
+
+                if left:
+                    self.__add_node(node=left)
+                if right:
+                    self.__add_node(node=right)
+
+            elif value < node.root_value:
+
+                if node.left == None:
+                    return None
+
+                if (node.left.root_value == value) and (node.left.left == None and node.left.right == None):
+                    node.left = None
+                else:
+                    return recursion(node=node.left, value=value)
+            elif value >= node.root_value:
+
+                if node.right == None:
+                    return None
+
+                if (node.right.root_value == value) and (node.right.left == None and node.right.right == None):
+                    node.right = None
+                else:
+                    return recursion(node=node.right, value=value)
+        
+        recursion(node=self, value=value)
